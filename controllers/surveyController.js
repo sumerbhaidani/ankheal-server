47e6ac44625a2abf2358c5d746bfbae0c41967e3
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-03-19 10:08:33 -0600

    Question 1 & 2 Logic Complete

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -30,16 +30,40 @@ export const sendResponse = async (req, res) => {
 export const getExerciseList = async (req, res) => {
   try {
     const exerciseList = await db("exercises");
-    const surveyResult = await db("surveys"); /*.where({
-      survey_id: req.params.id,
-    });*/
+    const surveyResult = await db("surveys");
+
     const singleSurvey = surveyResult.find(
       (survey) => survey.survey_id === req.params.id
     ); // able to isolate single survey
     // console.log(singleSurvey.survey_tags);
-    // for (i = 0; i < singleSurvey.survey_tags; i++) {
-    //   let;
-    // }
+    // console.log(exerciseList);
+    // let allExerciseTags = exerciseList.map((each) => each.tags);
+    // console.log(allExerciseTags);
+
+    // Q1 - Done
+    // need to validate if tag includes standing and sitting
+    let filteredafterQ1 = [];
+    if (singleSurvey.survey_tags.includes("Sitting")) {
+      filteredafterQ1 = exerciseList.filter((each) =>
+        each.tags.includes("Sitting")
+      );
+    } else {
+      filteredafterQ1 = exerciseList.filter((each) =>
+        each.tags.includes("Standing")
+      );
+    }
+
+    // Q2 - Done
+    let filteredAfterQ2 = [];
+    if (singleSurvey.survey_tags.includes("One leg balance")) {
+      filteredAfterQ2 = filteredafterQ1.filter((each) =>
+        each.tags.includes("One leg balance")
+      );
+    } else {
+      filteredAfterQ2 = filteredafterQ1;
+    }
+
+    console.log(filteredAfterQ2);
     res.status(200).json(singleSurvey);
   } catch (error) {
     res.status(500).json({ message: `Unable to recieve data for survey id` });
